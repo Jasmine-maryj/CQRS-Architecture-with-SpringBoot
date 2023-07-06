5594a8b175394ced4d3715c40d422897a52e15c1
Jasmine Mary <jasminemary630@gmail.com>
2023-07-06 20:48:12 +0530

    Base files added

---
+++ src/main/java/com/dev/jasmine/cqrsarchitecture/CqrsArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.dev.jasmine.cqrsarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/jasmine/cqrsarchitecture/CqrsArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.jasmine.cqrsarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
